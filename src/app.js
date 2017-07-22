21c0d12a2e24238404a8fbf9b3ad52d9a6c1b8dc
Steven Gusev <stephen.gusev@gmail.com>
2017-07-23 05:34:51 +0700

    Main page complete markup

--- src/app.js
+++ src/app.js
@@ -2,15 +2,57 @@ import './styles/app.sass';
 import React from 'react';
 import {render} from 'react-dom';
 
-class CafePreview extends React.Component {
-	render() {
-		var photoUrl = require("./img/cafe/"+ this.props.name + "/preview.jpg")
+var CafePreview = require('./components/cafe-preview.js').CafePreview;
 
+const CITY_LIST = {
+	tomsk: {
+		title: 'Томск'
+	},
+	novosibirsk: {
+		title: 'Новосибирск'
+	},
+	omsk: {
+		title: 'Омск'
+	}
+}
+
+const CAFE_LIST = [
+	{
+		name: "endorphin",
+		city: "tomsk",
+		title: "Эндорфин"
+	},
+	{
+		name: "skuratov",
+		city: "omsk",
+		title: "Скуратов"
+	},
+	{
+		name: "blackwood",
+		city: "novosibirsk",
+		title: "Blackwood"
+	}
+]
+
+class CafePreviewList extends React.Component {
+	constructor(props) {
+    super(props);
+		this.cafeList = this.props.cafes.map((cafe) =>
+			<CafePreview key={cafe.name} title={cafe.title} city={CITY_LIST[cafe.city].title} name={cafe.name} />
+		)
+  }
+
+	render() {
 		return (
-			<div className="cafe-preview">
-				<div className="photo" style={{ backgroundImage: `url(${photoUrl})` }}></div>
-				<div className="description">
-					<span className="title">{this.props.title}</span>, <span className="city">{this.props.city}</span>
+			<div className="cafe-preview-list">
+				<div className="cafe-filter">
+					<button className="active">Все города</button>
+					<button>Новосибирск</button>
+					<button>Томск</button>
+					<button>Омск</button>
+				</div>
+				<div className="cafe-list">
+					{this.cafeList}
 				</div>
 			</div>
 		)
@@ -25,16 +67,13 @@ class App extends React.Component {
 					<div className="logo">Raf, please</div>
 				</div>
 				<div className="section section-content section-list">
-					<div className="cafe-filter">
-						<div className="active">Все города</div>
-						<div>Новосибирск</div>
-						<div>Томск</div>
-						<div>Омск</div>
+					<CafePreviewList cafes={CAFE_LIST}/>
+				</div>
+				<div className="section section-content section-footer">
+					<div className="description">
+						<p><strong>Raf, please</strong> (Раф, плиз) рассказывает о лучших кафе городов Сибири. Не знаете куда сходить? Надоели старые места? Марина и Стив рассказывают только о тех заведениях, которые им нравятся.</p>
 					</div>
-					<div className="cafe-list">
-						<CafePreview title="Эндорфин" city="Томск" name="endorphin" />
-						<CafePreview title="Скуратов" city="Омск" name="skuratov" />
-					</div>
+					<div className="made-by">Pure &amp; Painful</div>
 				</div>
 			</div>
 		);
