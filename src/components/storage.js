25f9b4e4701319be3eee910efb60017dc2f48229
Steven Gusev <stephen.gusev@gmail.com>
2017-07-25 09:40:10 +0700

    Splitted places into separate files

--- src/components/storage.js
+++ src/components/storage.js
@@ -1,83 +1,10 @@
-const PLACE_DICTIONARY = {
-	endorphin: {
-		city: 'tomsk',
-		title: 'Эндорфин',
-		address: 'Учебная 2',
-		workingHours: {
-			start: '10:00',
-			end: '20:00'
-		},
-		lastUpdate: '4 июля 2017',
-		gisLink: 'https://2gis.ru/tomsk/query/ЭНDОРФИН%2C%20сеть%20микрокофеен/firm/70000001023564244?queryState=center%2F84.942545%2C56.464132%2Fzoom%2F16',
-		tags: ['Кофе', 'Поработать', 'Wi-fi'],
-		mentions: {
-			steven: 'Люблю заходить сюда выпить кофе. Не только кафе, но и их киоски — кофе всегда отличный.',
-			marina: 'Люблю здесь раф — один из самых лучших в городе. Ещё нравится что каждый раз дают печеньку. Хоть я их и не ем, всё равно приятно.'
-		},
-		dishes: [
-			{
-				title: 'Классический раф',
-				price: '160 ₽',
-				text: 'Лучший раф в городе. В меру сладкий, в меру горький. Можно ходить только ради него.',
-				name: 'classic-raf'
-			},
-			{
-				title: 'Кедровый латте',
-				price: '160 ₽',
-				text: 'Лучший раф в городе. В меру сладкий, в меру горький. Можно ходить только ради него.',
-				name: 'cedric-latte'
-			},
-			{
-				title: 'Флет вайт',
-				price: '160 ₽',
-				text: 'Лучший раф в городе. В меру сладкий, в меру горький. Можно ходить только ради него.',
-				name: 'flat-white'
-			},
-		],
-		page: [
-			{
-				type: 'intro',
-				text: [
-					'«Эндорфин» — это лучший кофе в Томске. Кафе маленькое, не больше 20 мест. Тут уютная атмосфера, вкусный кофе и невероятная деревянная конструкция на стене.',
-					'Помимо кафе, у них есть ещё две мини-кофейни — на ТГУ и на Лагерном.'
-				]
-			},
-			{
-				type: 'left-photo',
-				photo: 'places-to-sit',
-				text: 'В кафе довольно мало мест, и все они не сильно удобны для долгой работы.'
-			},
-			{
-				type: 'two-photos',
-				photos: [
-					{
-						photo: 'stairs',
-						text: 'Ещё можно сидеть на гигантских ступенях с подушками!'
-					},
-					{
-						photo: 'chairs',
-						text: 'Есть два кресла, на которых можно вести непринуждённую беседу или читать книгу.'
-					}
-				]
-			},
-			{
-				type: 'full-width-photo',
-				photo: 'coffee-machine',
-				text: 'Гигантская красная кофемашина сияет на всю кофейню.'
-			}
-		]
-	},
-	skuratov: {
-		city: 'omsk',
-		title: 'Скуратов'
-	},
-	blackwood: {
-		city: 'novosibirsk',
-		title: 'Blackwood'
-	}
+var PLACE_ENDORPHIN = require('./storage/places/endorphin.js').ENDORPHIN;
+
+const PLACES = {
+	endorphin: PLACE_ENDORPHIN
 }
 
-const CITY_DICTIONARY = {
+const CITIES = {
 	tomsk: {
 		title: 'Томск'
 	},
@@ -90,7 +17,7 @@ const CITY_DICTIONARY = {
 }
 
 var getPlaceDictionary = function() {
-	return PLACE_DICTIONARY
+	return PLACES
 }
 
 var getPlace = function(name) {
@@ -129,7 +56,7 @@ var getPlaceShortList = function() {
 }
 
 var getCityDictionary = function() {
-	return CITY_DICTIONARY
+	return CITIES
 }
 
 var getCityList = function(includeAllCities) {
