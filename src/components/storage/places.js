b89014030ccf0c09d72df3cbebd7af6444c17dbf
Steven Gusev <stephen.gusev@gmail.com>
2017-07-31 20:50:22 +0700

    Blackwood page draft

--- src/components/storage/places.js
+++ src/components/storage/places.js
@@ -3,11 +3,13 @@ const getCityTitle = require('./cities.js').getCityTitle;
 const PLACE_ENDORPHIN = require('./places/endorphin.js').ENDORPHIN;
 const PLACE_PITCHII = require('./places/pitchii.js').PITCHII;
 const PLACE_MAKELOVEPIZZA = require('./places/makelovepizza.js').MAKELOVEPIZZA;
+const PLACE_BLACKWOOD = require('./places/blackwood.js').BLACKWOOD;
 
 const PLACES = {
 	endorphin: PLACE_ENDORPHIN,
 	pitchii: PLACE_PITCHII,
-	makelovepizza: PLACE_MAKELOVEPIZZA
+	makelovepizza: PLACE_MAKELOVEPIZZA,
+	blackwood: PLACE_BLACKWOOD
 }
 
 var getPlaceDictionary = function() {
