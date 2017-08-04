7441cc1fe0c9e54215c25f12675a56fcfe533c20
Steven Gusev <stephen.gusev@gmail.com>
2017-08-05 01:03:27 +0700

    Place preview list static render

--- src/components/place-preview-list.js
+++ src/components/place-preview-list.js
@@ -1,20 +1,15 @@
 import React from 'react';
 import PlacePreview from './place-preview-list/place-preview.js';
 
-const getPlaces = require('./storage/places.js').getPlaceShortList;
-const getCityTitle = require('./storage/cities.js').getCityTitle;
-
 class PlacePreviewList extends React.Component {
 	render() {
-		var places = getPlaces();
-		var placeList = places.map((place) =>
-			<PlacePreview key={place.name} title={place.title} city={getCityTitle(place.city)} name={place.name} />
-		)
-
 		return (
 			<div className="place-preview-list">
-				<div className="place-list">
-					{placeList}
+				<div className="place-preview-list-city">
+					<h2>Новосибирск</h2>
+					<div className="big-place">
+						<PlacePreview title="Blackwood" city="Новосибриск" name="blackwood" />
+					</div>
 				</div>
 			</div>
 		)
