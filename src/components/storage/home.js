a3fd3f022103ede868521cd15034c5bcbfc10fec
Steven Gusev <stephen.gusev@gmail.com>
2017-08-05 00:50:13 +0700

    Home page layout json

---
+++ src/components/storage/home.js
@@ -0,0 +1,33 @@
+const SCENE_HOME = [
+	{
+		cityName: 'Новосибирск',
+		items: [
+			{
+				type: 'big-place',
+				place: 'blackwood'
+			},
+			{
+				type: 'two-places',
+				places: [
+					'barbq',
+					'pitchii'
+				]
+			}
+		]
+	},
+	{
+		cityName: 'Томск',
+		items: [
+			{
+				type: 'big-place',
+				place: 'makelovepizza'
+			},
+			{
+				type: 'left-place',
+				place: 'endorphin'
+			}
+		]
+	}
+]
+
+module.exports = SCENE_HOME;
