7ef7d99357430177abdd1afd6e3953ff17975c20
Steven Gusev <stephen.gusev@gmail.com>
2017-08-08 00:47:56 +0700

    Block with place on the left.

--- src/components/place-preview-list.js
+++ src/components/place-preview-list.js
@@ -15,6 +15,18 @@ class PlacePreviewList extends React.Component {
 						<PlacePreview name="pitchii" />
 					</div>
 				</div>
+				<div className="place-preview-list-city">
+					<h2>Томск</h2>
+					<div className="big-place">
+						<PlacePreview name="endorphin" />
+					</div>
+					<div className="place-on-left">
+						<PlacePreview name="makelovepizza" />
+						<div className="place-preview-list-instagram">
+							Аппетитный инстаграм это <a href="http://instagram.com/rafplease">@rafplease</a>
+						</div>
+					</div>
+				</div>
 			</div>
 		)
 	}
